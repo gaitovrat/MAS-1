397eff22d18a49d3717955d05eef324d7dbd6c42
Ratmir Gaitov <gaitovrat@gmail.com>
2024-12-31 14:32:39 +0100

    Fix setup.py problems

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MAS 1
 Metody analýzy sítí I
+
+## Install dependencies
+Create env from `environment.yml` using `conda`:
+```sh
+conda env create --name mas1 --file environment.yaml
+```
--- setup.py
+++ setup.py
@@ -1,30 +1,9 @@
-from setuptools import setup, find_packages
-import yaml
 import os
 from pathlib import Path
 
+from setuptools import find_packages, setup
+
 FILE_DIR = Path(os.path.dirname(os.path.abspath(__file__)))
-
-
-def load_dependencies() -> list[str]:
-    out: list[str] = []
-    dependencies: list[str]
-
-    with open(FILE_DIR / "environment.yml", "r", encoding="utf-8") as fd:
-        config = yaml.safe_load(fd)
-
-    dependencies = config.get("dependencies", [])
-    if not dependencies:
-        return []
-
-    dependencies = dependencies[1:]  # Skip "python=3.12"
-    for dependency in dependencies:
-        if isinstance(dependency, dict):
-            dependencies.extend(dependency.get("pip", []))
-        else:
-            out.append(dependency)
-
-    return out
 
 
 setup(
@@ -35,7 +14,6 @@ setup(
     long_description_content_type="text/markdown",
     packages=find_packages(),
     python_requires=">=3.6",
-    install_requires=load_dependencies(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
