692e99f6a6a68cbc069fac06a3b394efb28fca29
Ratmir Gaitov <gaitovrat@gmail.com>
2025-01-07 14:54:45 +0000

    Run pre-commit

--- mas/adjacency.py
+++ mas/adjacency.py
@@ -7,7 +7,7 @@ def adjacency_matrix(filename: str) -> ndarray:
     edges = []
 
     with open(filename, "r") as fd:
-        reader = csv.reader(fd, delimiter=';')
+        reader = csv.reader(fd, delimiter=";")
         for e1, e2 in reader:
             edges.append((int(e1), int(e2)))
 
@@ -15,7 +15,7 @@ def adjacency_matrix(filename: str) -> ndarray:
     data = zeros((max_node, max_node), dtype=int)
 
     for u, v in edges:
-        data[u-1, v-1] = data[v-1, u-1] = 1
+        data[u - 1, v - 1] = data[v - 1, u - 1] = 1
 
     return data
 
@@ -24,10 +24,15 @@ def adjacency_list(filename: str) -> ndarray:
     data = []
 
     with open(filename, "r") as fd:
-        reader = csv.reader(fd, delimiter=';')
+        reader = csv.reader(fd, delimiter=";")
         for line in reader:
             i = int(line[0])
             j = int(line[1])
-            data.append((i, j,))
+            data.append(
+                (
+                    i,
+                    j,
+                )
+            )
 
     return array(data, dtype=int)
